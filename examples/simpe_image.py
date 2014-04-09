d5010908649c94993643c285be3b90648384053d
Sorina Sandu <sandu.sorina@gmail.com>
2014-04-09 23:22:41 +0300

    Add a simple window with a image inside

---
+++ examples/simpe_image.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+
+white = (255, 255, 255)
+window_size = (400, 400)
+
+
+if __name__ == '__main__':
+
+    pygame.init()
+
+    screen = pygame.display.set_mode(window_size, DOUBLEBUF)
+    screen.fill(white)
+
+    tux = pygame.image.load('tux.png')
+    tux = pygame.transform.scale(tux, window_size)
+
+    screen.blit(tux, (0, 0))
+    pygame.display.flip()
+
+    running = True
+    while running:
+        event = pygame.event.wait()
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.quit()
