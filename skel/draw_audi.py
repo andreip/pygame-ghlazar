f89c5e0c80a71c92bd209d6e1d6283a79e3d75f5
Sorina Sandu <sandu.sorina@gmail.com>
2014-04-10 03:04:52 +0300

    Add task for drawing the Audi logo

---
+++ skel/draw_audi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *
+
+white = (255, 255, 255)
+
+# TODO: define grey.
+
+
+if __name__ == '__main__':
+
+    screen = pygame.display.set_mode((800, 400), DOUBLEBUF)
+    screen.fill(white)
+
+
+    # TODO: draw the Audi logo. You can search for a picture online.
+
+
+    pygame.display.flip()
+
+    running = True
+    while running:
+        event = pygame.event.wait()
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.quit()
